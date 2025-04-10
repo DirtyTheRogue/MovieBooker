7dfd25be9066aa7f93187b7779e3cfdc68a4e126
DrtyTheRogue <charles.chachouyi@gmail.com>
2025-04-10 15:09:50 +0200

    modif env / app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,11 +22,7 @@ import { User } from './user/user.entity';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
       type: 'postgres',
-      host: configService.get('DB_HOST'),
-      port: configService.get('DB_PORT'),
-      username: configService.get('DB_USERNAME'),
-      password: configService.get('DB_PASSWORD'),
-      database: configService.get('DB_NAME'),
+      url: configService.get('DATABASE_URL'), 
       autoLoadEntities: true,
       entities: [User, Reservation],
       synchronize: true,
